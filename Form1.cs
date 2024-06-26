b557dbe627dde28fd9d66e17eb99dd63588d356d
DJ03MT <144065771+DJ03MT@users.noreply.github.com>
2024-06-26 17:18:19 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace Barberia
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("insert into ut values(@id,@descripcion,@monto)",con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@descripcion", int.Parse(textBox2.Text));
+            cmd.Parameters.AddWithValue("@monto", float.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Agregado correctamente");
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("update ut set descripcion = @descripcion, monto = @monto where id = @id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.Parameters.AddWithValue("@descripcion", int.Parse(textBox2.Text));
+            cmd.Parameters.AddWithValue("@monto", float.Parse(textBox3.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Actualizado correctamente");
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete ut where id = @id", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            cmd.ExecuteNonQuery();
+            con.Close();
+            MessageBox.Show("Borrado Exitosamente!");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection("");
+            con.Open();
+            SqlCommand cmd = new SqlCommand("Select * from ut ", con);
+            cmd.Parameters.AddWithValue("@id", int.Parse(textBox1.Text));
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
